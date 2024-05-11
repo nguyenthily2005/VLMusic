5db051205aab9764dd0440ece322a8503b5af6d1
nguyenthily2005 <lythinguyen2005@gmail.com>
2024-05-12 00:19:54 +0700

    GUIAlbumPane

--- src/main/java/DAO/SongDAO.java
+++ src/main/java/DAO/SongDAO.java
@@ -65,7 +65,7 @@ public class SongDAO {
             session.beginTransaction();
 
             Query<SongsEntity> query = session.createQuery("from SongsEntity order by playCount desc", SongsEntity.class);
-            query.setMaxResults(25);
+            query.setMaxResults(35);
             List<SongsEntity> songs = query.list();
 
             session.getTransaction().commit();
