0c265d0b968e07b75e68946e92fce836afb001b4
nguyenthily2005 <lythinguyen2005@gmail.com>
2024-05-17 21:24:26 +0700

    AddAlbum

--- src/main/java/GUI/App.java
+++ src/main/java/GUI/App.java
@@ -14,13 +14,14 @@ import java.util.Stack;
 
 public class App extends Application {
     public static Stage stage;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("dash-board.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
         stage.setTitle("VibeLab");
         stage.setScene(scene);
-stage.getIcons().add(new Image(getClass().getResource("/music/img/logoVL.png").toString()));
+        stage.getIcons().add(new Image(getClass().getResource("/music/img/logoVL.png").toString()));
         stage.show();
         this.stage = stage;
     }
